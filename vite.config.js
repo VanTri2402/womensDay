53611491ae7007f57744d0ed2ca6c48b789b2302
VanTri <99nguyenhuuvantri2023@gmail.com>
2025-03-08 20:51:42 +0700

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/womensDay/", // Đổi theo tên repo của bạn
+
   plugins: [react()],
 });
